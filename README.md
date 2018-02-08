383f24d84d0d86650e8ef01c3462a8b2e6012265
Mihai Tiuca <teodor.tiuca@gmail.com>
2018-02-07 19:29:02 -0500

    Add README, run.sh - exit sim test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # droneCode
+
+RUN:
+
+```
+./install.sh
+. ~/.bashrc
+./run.sh
+```
--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 # Reset Sim Parameters
 cd ~/ardupilot/ArduCopter
-sim_vehicle.py -w
-exit
+sim_vehicle.py -w &
+export pid=$!
+sleep 10
+kill -2  $pid
 
 sim_vehicle.py --console --map --aircraft test
 wp load ../Tools/autotest/ArduPlane-Missions/CMAC-toff-loop.txt
