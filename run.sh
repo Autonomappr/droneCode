c724499539a618f6e1d79d1f07954386ab16e2fc
Mihai Tiuca <teodor.tiuca@gmail.com>
2018-02-07 19:35:20 -0500

    run.sh - test

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@
 cd ~/ardupilot/ArduCopter
 sim_vehicle.py -w &
 export pid=$!
-sleep 10
+sleep 100
 kill -2  $pid
 
 sim_vehicle.py --console --map --aircraft test
+sleep 20
 wp load ../Tools/autotest/ArduPlane-Missions/CMAC-toff-loop.txt
 arm throttle
 mode auto
